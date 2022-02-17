6860975abbbf59b8d7c1cc43d73ab59ef0daa0fe
Zeiba <96964890+I-UwU-You@users.noreply.github.com>
2022-02-17 15:25:08 +1030

    RATS

--- filetres.c
+++ filetres.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		printf("Well done\n");
-		printf("There was no purpose to this\n");
+		printf("There was no RATS in this\n");
 	}	
 	return (0);
 }	
